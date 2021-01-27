c15f15c56526f2ddd114c5ae3e7aa1fd825f48e4
Dmitryitis <dmitry.itis.gerasimov@gmail.com>
2021-01-27 23:38:22 +0300

    add

--- NewsPortal/personalcabinet/urls.py
+++ NewsPortal/personalcabinet/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from personalcabinet.views import main_personal
+from personalcabinet.views import main_personal, cabinet_posts, write_post
 
 urlpatterns = [
     path('cabinet/', main_personal, name='main_personal'),
+    path('cabinetposts/', cabinet_posts, name='cabinet_posts'),
+    path('writepost/', write_post, name="write_post")
 ]
--- NewsPortal/personalcabinet/views.py
+++ NewsPortal/personalcabinet/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def main_personal(request):
     return render(request, 'personalCabinet.html', )
+
+
+def cabinet_posts(request):
+    return render(request, 'personal-allpost.html')
+
+
+def write_post(request):
+    return render(request, 'writepost.html')
